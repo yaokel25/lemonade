cdb8be6f1fe531a59ad215288d3f541ec6398f0f
yaokel25 <yaokel25@7hills.org>
2023-09-18 13:12:48 -0400

    create new files

---
+++ Lemonade.java
@@ -0,0 +1,11 @@
+public class MainLemonade {
+
+    public static void main(String[] args){
+        Instructions show = new Instructions();
+
+        show.Instructions("start");
+
+
+
+    }
+    }
---
+++ Setup.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Setup{
+    double lemon;
+    double cup;
+    double ice;
+    double sugar;
+    double budget;
+    System.out.println("How many cups? ($0.01 each)");
+    cup = scan.nextLine();
+    System.out.println("How many lemons? ($0.10 each)");
+    lemon = scan.nextLine();
+    System.out.println("How much ice? ($0.01 each)");
+    ice = scan.nextLine();
+    System.out.println("How much sugar? ($0.25 each)");
+    sugar = scan.nextLine();
+
+    budget = 20 - (0.01*cup) - ()
+
+
+}
